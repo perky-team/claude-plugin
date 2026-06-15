8b5208251a670229e47c2e731e01550dc3517118
Andrey Sukharev <andrey.sukharev@mail.com>
2026-06-15 20:06:28 +0300

    feat(p-graph): Python tags queries

--- plugins/p-graph/tools/lib/parse/lang/py.scm
+++ plugins/p-graph/tools/lib/parse/lang/py.scm
@@ -1,1 +1,7 @@
-;; placeholder
+(function_definition name: (identifier) @name) @definition.function
+(class_definition name: (identifier) @name) @definition.class
+
+(call function: (identifier) @reference.call)
+(call function: (attribute attribute: (identifier) @reference.call))
+(import_statement name: (dotted_name) @reference.import)
+(import_from_statement module_name: (dotted_name) @reference.import)
